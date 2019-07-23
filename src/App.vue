5c78f8aa177f37174fd2e88a71a0e86592d04a58
Svetlana Tovt <ms.tsp00@gmail.com>
2019-07-23 08:49:31 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+}
+</script>
+
+<style lang="scss">
+#app {
+  height: 100%;
+  width: 100%;
+  text-align: center;
+}
+</style>
