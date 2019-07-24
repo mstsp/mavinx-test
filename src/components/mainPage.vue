51fa9858f291ecf606ace6f772162238d3d9722a
Svetlana Tovt <ms.tsp00@gmail.com>
2019-07-25 01:14:54 +0300

    Add profiles

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="container">main-page</div>
+  <div class="container">
+    main-page
+    <button v-on:click="getUserRole()">Click</button>
+  </div>
 </template>
 
 <script>
+import profileSupplier from "./profileSupplier.vue";
+import profileCustomer from "./profileCustomer.vue";
+
 export default {
   data() {
-    return {};
+    return {
+      userRole: ""
+    };
+  },
+  components: {
+    profileSupplier,
+    profileCustomer
+  },
+  methods: {
+    getUserRole() {
+      this.userRole = JSON.parse(
+        JSON.stringify(localStorage.getItem("userRole"))
+      );
+
+        this.$router.push(`/profile-${this.userRole}`);
+    }
   }
 };
 </script>
@@ -17,6 +38,5 @@ export default {
   height: 100%;
   width: 100%;
 }
-
 </style>
 
--- src/router.js
+++ src/router.js
@@ -3,6 +3,9 @@ import Router from "vue-router";
 import registration from "./components/registrationForm.vue";
 import authorization from "./components/authorization.vue";
 import mainPage from "./components/mainPage.vue";
+import profileSupplier from './components/profileSupplier.vue';
+import profileCustomer from './components/profileCustomer.vue'
+
 
 Vue.use(Router);
 
@@ -25,6 +28,18 @@ export default new Router({
       path: '/main',
       name: 'main',
       component: mainPage,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/profile-supplier',
+      name: 'profileSupplier',
+      component: profileSupplier,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/profile-customer',
+      name: 'profileCustomer',
+      component: profileCustomer,
       meta: { requiresAuth: true },
     }
   ]
